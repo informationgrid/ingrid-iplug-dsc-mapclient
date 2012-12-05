902600493c2b966ff09309146fd44756bb2b3d46
ma <ma@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2012-12-05 16:30:34 +0000

    dsc-mapclient - corrected a typo

--- src/main/release/scripts/start.sh
+++ src/main/release/scripts/start.sh
@@ -31,7 +31,7 @@ if [ -f $INGRID_HOME/conf/plugdescription.xml ]; then
 fi
 
 # include a debug script, if available, i.e. to specify debug port, etc.
-# caution: the debug script must echo the actual command to be able to work in the current environment
+# caution: the jmx script must echo the actual command to be able to work in the current environment
 if [ -f $INGRID_HOME/debug.sh ]; then
   eval `sh $INGRID_HOME/debug.sh`
 fi
